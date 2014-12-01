16c500e35c2d7b29c0c165bc2695aeaf232047c6
Ryan LaBouve <imryanlabouve@gmail.com>
2014-11-30 22:58:04 -0600

    Display the number of incomplete todos

--- app/controllers/todos.js
+++ app/controllers/todos.js
@@ -17,5 +17,13 @@ export default Ember.ArrayController.extend({
 
       todo.save();
     }
-  }
+  },
+  remaining: function() {
+    return this.filterBy('isCompleted', false).get('length');
+  }.property('@each.isCompleted'),
+
+  inflection: function() {
+    var remaining = this.get('remaining');
+    return remaining === 1 ? 'item' : 'items';
+  }.property('remaining')
 })
