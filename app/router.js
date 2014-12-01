8275c5b63b4ef833b3980b1662ef5b5f846b0e5d
Ryan LaBouve <imryanlabouve@gmail.com>
2014-12-01 01:24:04 -0600

    Transitioning to show only complete todos

--- app/router.js
+++ app/router.js
@@ -8,6 +8,7 @@ var Router = Ember.Router.extend({
 Router.map(function() {
   this.resource('todos', { path: '/' }, function() {
     this.route('active');
+    this.route('completed');
   });
 });
 
