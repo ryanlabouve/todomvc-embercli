9d096bc63317d335f015de79654e3dd4d9823198
Ryan LaBouve <imryanlabouve@gmail.com>
2014-11-30 22:44:58 -0600

    Mark item as complete or incomplete

---
+++ app/controllers/todo.js
@@ -0,0 +1,18 @@
+import Ember from 'ember';
+
+export default Ember.ObjectController.extend({
+  isCompleted: function(key, value) {
+    // what is this?
+    var model = this.get('model');
+
+    if (value === undefined) {
+      // property being used as a getter
+      return model.get('isCompleted');
+    } else {
+      // property being used as a setter
+      model.set('isCompleted', value);
+      model.save();
+      return value;
+    }
+  }.property('model.isCompleted')
+});
