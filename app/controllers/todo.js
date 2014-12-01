2638bc5cb2b3d51a402b63fb912a237195556162
Ryan LaBouve <imryanlabouve@gmail.com>
2014-11-30 23:27:38 -0600

    Accept edits

--- app/controllers/todo.js
+++ app/controllers/todo.js
@@ -4,6 +4,20 @@ export default Ember.ObjectController.extend({
   actions: {
       editTodo: function() {
         this.set('isEditing', true);
+      },
+      acceptChanges: function() {
+        this.set('isEditing', false);
+
+        if (Ember.isEmpty(this.get('model.title'))) {
+          this.send('removeTodo');
+        } else {
+          this.get('model').save();
+        }
+      },
+      removeTodo: function() {
+        var todo = this.get('model');
+        todo.deleteRecord();
+        todo.save();
       }
   },
 
