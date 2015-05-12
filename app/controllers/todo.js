465bc65ac64ae17e87d7426068d50563e179aa3c
Brandon L. Gauthier <Brandon.L.Gauthier@gmail.com>
2015-05-11 22:37:22 -0400

    Removed deprecated ObjectController
    * Updated ObjectController to Controller
    * Update template to reference model

--- app/controllers/todo.js
+++ app/controllers/todo.js
@@ -1,6 +1,6 @@
 import Ember from 'ember';
 
-export default Ember.ObjectController.extend({
+export default Ember.Controller.extend({
   actions: {
       editTodo: function() {
         this.set('isEditing', true);
